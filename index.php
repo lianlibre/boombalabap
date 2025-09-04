45ccc9bedf7c9fbcda2487885bc9e44335a9ead2
rose <lianlibre491@gmail.com>
2025-09-04 17:18:38 +0800

    ble

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 // Redirect to login page
-header("Location: /public/frontend/login.php");
+// header("Location: /public/frontend/login.php");
+header("Location: https://https://www.youtube.com/watch?v=SMbKW7OtnQA");
 exit;
 ?>
