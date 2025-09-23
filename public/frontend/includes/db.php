98d219d4cfb5eed791b587e554b1fc3748a68651
rose <lianlibre491@gmail.com>
2025-09-24 04:31:20 +0800

    a

--- public/frontend/includes/db.php
+++ public/frontend/includes/db.php
@@ -1,11 +1,11 @@
 <?php
-/* $conn = new mysqli("localhost", "u802714156_mccMemoPass", "MemoPass2025", "u802714156_mccMemo");
+ $conn = new mysqli("localhost", "u802714156_mccMemoPass", "MemoPass2025", "u802714156_mccMemo");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+/*    $conn = new mysqli("localhost", "root", "", "generator_db");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } */
-
-    $conn = new mysqli("localhost", "root", "", "generator_db");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
