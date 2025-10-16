56355b90dcc47d3c238010cba98f15ffa63ecbce
rose <lianlibre491@gmail.com>
2025-10-17 05:10:54 +0800

    a

--- public/frontend/includes/admin_sidebar.php
+++ public/frontend/includes/admin_sidebar.php
@@ -329,7 +329,7 @@ if (isset($_SESSION['admin_id'])) {
     </div>
 
     <div class="sidebar-actions">
-        <a href="profile" class="fas fa-user">Profile</a>
+        <a href="profile" class="btn">👤</a>
         <?php if (current_user_can('can_create_memo')): ?>
             <a href="memo_add" class="btn">+ Add Memos</a>
         <?php endif; ?>
